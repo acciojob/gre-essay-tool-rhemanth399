71cf664c28fbc7294efb247bdd82e7d48c2a8e96
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-21 16:31:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let input=document.getElementById('evaluatedText')
+let heading3=document.getElementById('wordCount')
+input.addEventListener('input',essay)
+function essay() {
+	heading3=input.value.length
+}
