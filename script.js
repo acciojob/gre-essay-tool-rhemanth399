463b275a9c9331bf1c9f83d2465ebe72377420b0
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-23 18:40:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your code here
-const textbox = document.getElementById('evaluatedText');
-const letterCountElement = document.getElementById('wordCount');
 
-textbox.addEventListener('keyup', function() {
-  const letterCount = textbox.value.length;
-  letterCountElement.innerHTML = letterCount;
+const textbox = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+textbox.addEventListener("input", () => {
+  wordCount.textContent = textbox.value.split(" ").filter(x => x.length > 0).length;
 });
